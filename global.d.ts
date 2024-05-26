a5de2262f2645bc7ed37d4e40321e9981310b764
Harold Anderson <harold@acfo.co>
2024-05-26 13:27:01 -0700

    added stuff to typscript to get netlify to work.

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+// src/types/global.d.ts
+declare global {
+    interface Window {
+      netlifyIdentity: any; // Replace 'any' with a more specific type if possible
+    }
+  }
+  
+  export {};
+  
